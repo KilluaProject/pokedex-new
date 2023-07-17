fce50a16d88d93f59904aa6620a32b3634905d09
Raden Agung Prakoso Wibowo <134472737+KilluaProject@users.noreply.github.com>
2023-07-17 10:22:22 +0700

    Add files via upload
    adding Load more

--- app/components/PokemonGrid.jsx
+++ app/components/PokemonGrid.jsx
@@ -7,8 +7,6 @@ import Link from "next/link";
 
 export default function PokemonGrid({ pokemonList }) {
   const [searchText, setSearchText] = useState("");
-  const [pokemon, setPokemon] = useState(pokemonList)
-  const [offset, setOffset] = useState(0)
  
   const searchFilter = (pokemonList) => {
     return pokemonList.filter((pokemon) =>
@@ -17,15 +15,6 @@ export default function PokemonGrid({ pokemonList }) {
   };
 
   const filteredPokemonList = searchFilter(pokemonList);
-
-
-  const fetchPokemon = async (pokemonList, next) => {
-      const response = await fetch(pokemonList)
-      const nextPokemon = await response.json()
-
-      setOffset(next ? offset + 20 : offset - 20)
-      setPokemon(nextPokemon)
-  }
 
   return (
     <div className="flex flex-col pt-36">
@@ -40,7 +29,6 @@ export default function PokemonGrid({ pokemonList }) {
       </div>
       <div className="grid sm:grid-cols-4 gap-5">
         {filteredPokemonList.map((pokemon) => {
-            const id = pokemon.url.split('/')[6]
             return (
               
                 <Link href={pokemon.name} className="flex flex-col items-center justify-center border p-4 rounded-lg " key={pokemon.name}>
@@ -50,7 +38,7 @@ export default function PokemonGrid({ pokemonList }) {
                         <Image
                         width={200}
                         height={200}
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                        src={pokemon.sprites.other['official-artwork'].front_default}
                         alt={pokemon.name}/>
                     
                     
@@ -58,12 +46,6 @@ export default function PokemonGrid({ pokemonList }) {
             )
             })}
       </div>
-      <div className="py-10 flex gap-2">
-
-      <button className="disabled:bg-gray-500 px-3 py-1 bg-slate-900" onClick={() => fetchPokemon(pokemon.previous, false)}>prev</button>
-      <button className="disabled:bg-gray-500 px-3 py-1 bg-slate-900" onClick={() => fetchPokemon(pokemon.next, true)}>next</button>
-   
-      </div>
-    </div>
+    </div> 
   );
 }
